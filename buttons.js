cb11eb03dd2a28ff66226e3523296ea546981795
chasebarkley <chasebarkley@users.noreply.github.com>
2019-04-03 20:14:06 -0500

    buttons upload

---
+++ buttons.js
@@ -0,0 +1,104 @@
+function appsFunction() {
+var x = document.getElementById("apps");
+if (x.style.display === "block") {
+    x.style.display = "none";
+} else {
+    x.style.display = "block";
+}
+}
+function plattersFunction() {
+var x = document.getElementById("chkPlat");
+if (x.style.display === "none") {
+    x.style.display = "block";
+} else {
+    x.style.display = "none";
+}
+}
+function steaksFunction() {
+var x = document.getElementById("steak");
+if (x.style.display === "block") {
+    x.style.display = "none";
+} else {
+    x.style.display = "block";
+}
+}
+function seaFunction() {
+var x = document.getElementById("sea");
+if (x.style.display === "block") {
+    x.style.display = "none";
+} else {
+    x.style.display = "block";
+}
+}
+function specialFunction() {
+var x = document.getElementById("special");
+if (x.style.display === "block") {
+    x.style.display = "none";
+} else {
+    x.style.display = "block";
+}
+}
+function burgerFunction() {
+var x = document.getElementById("burgers");
+if (x.style.display === "block") {
+    x.style.display = "none";
+} else {
+    x.style.display = "block";
+}
+}
+function sandFunction() {
+var x = document.getElementById("sand");
+if (x.style.display === "block") {
+    x.style.display = "none";
+} else {
+    x.style.display = "block";
+}
+}
+function saladFunction() {
+var x = document.getElementById("salad");
+if (x.style.display === "block") {
+    x.style.display = "none";
+} else {
+    x.style.display = "block";
+}
+}
+function lunchFunction() {
+var x = document.getElementById("lunch");
+if (x.style.display === "block") {
+    x.style.display = "none";
+} else {
+    x.style.display = "block";
+}
+}
+function sideFunction() {
+var x = document.getElementById("sides");
+if (x.style.display === "block") {
+    x.style.display = "none";
+} else {
+    x.style.display = "block";
+}
+}
+function psideFunction() {
+var x = document.getElementById("psides");
+if (x.style.display === "block") {
+    x.style.display = "none";
+} else {
+    x.style.display = "block";
+}
+}
+function comboFunction() {
+var x = document.getElementById("combo");
+if (x.style.display === "block") {
+    x.style.display = "none";
+} else {
+    x.style.display = "block";
+}
+}
+function wingFunction() {
+var x = document.getElementById("wings");
+if (x.style.display === "block") {
+    x.style.display = "none";
+} else {
+    x.style.display = "block";
+}
+}
