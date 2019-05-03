7e303462131c399a1106943c35e8e87326884dab
chasebarkley <chasebarkley@users.noreply.github.com>
2019-05-02 23:23:15 -0500

    code fix

--- buttons.js
+++ buttons.js
@@ -1,10 +1,10 @@
 function appsFunction() {
 var x = document.getElementById("apps");
-if (x.style.display === "block") {
-    x.style.display = "none";
-} else {
-    x.style.display = "block";
-}
+if (x.style.display === "none") {
+        x.style.display = "block";
+    } else {
+        x.style.display = "none";
+    }
 }
 function plattersFunction() {
 var x = document.getElementById("chkPlat");
@@ -16,89 +16,97 @@ if (x.style.display === "none") {
 }
 function steaksFunction() {
 var x = document.getElementById("steak");
-if (x.style.display === "block") {
-    x.style.display = "none";
-} else {
+if (x.style.display === "none") {
     x.style.display = "block";
+} else {
+    x.style.display = "none";
 }
 }
 function seaFunction() {
 var x = document.getElementById("sea");
-if (x.style.display === "block") {
-    x.style.display = "none";
-} else {
+if (x.style.display === "none") {
     x.style.display = "block";
+} else {
+    x.style.display = "none";
 }
 }
 function specialFunction() {
 var x = document.getElementById("special");
-if (x.style.display === "block") {
-    x.style.display = "none";
-} else {
+if (x.style.display === "none") {
     x.style.display = "block";
+} else {
+    x.style.display = "none";
 }
 }
 function burgerFunction() {
 var x = document.getElementById("burgers");
-if (x.style.display === "block") {
-    x.style.display = "none";
-} else {
+if (x.style.display === "none") {
     x.style.display = "block";
+} else {
+    x.style.display = "none";
 }
 }
 function sandFunction() {
 var x = document.getElementById("sand");
-if (x.style.display === "block") {
-    x.style.display = "none";
-} else {
+if (x.style.display === "none") {
     x.style.display = "block";
+} else {
+    x.style.display = "none";
 }
 }
 function saladFunction() {
 var x = document.getElementById("salad");
-if (x.style.display === "block") {
-    x.style.display = "none";
-} else {
+if (x.style.display === "none") {
     x.style.display = "block";
+} else {
+    x.style.display = "none";
 }
 }
 function lunchFunction() {
 var x = document.getElementById("lunch");
-if (x.style.display === "block") {
-    x.style.display = "none";
-} else {
+if (x.style.display === "none") {
     x.style.display = "block";
+} else {
+    x.style.display = "none";
 }
 }
 function sideFunction() {
 var x = document.getElementById("sides");
-if (x.style.display === "block") {
-    x.style.display = "none";
-} else {
+if (x.style.display === "none") {
     x.style.display = "block";
+} else {
+    x.style.display = "none";
 }
 }
 function psideFunction() {
 var x = document.getElementById("psides");
-if (x.style.display === "block") {
-    x.style.display = "none";
-} else {
+if (x.style.display === "none") {
     x.style.display = "block";
+} else {
+    x.style.display = "none";
 }
 }
 function comboFunction() {
 var x = document.getElementById("combo");
-if (x.style.display === "block") {
-    x.style.display = "none";
-} else {
+if (x.style.display === "none") {
     x.style.display = "block";
+} else {
+    x.style.display = "none";
 }
 }
 function wingFunction() {
 var x = document.getElementById("wings");
-if (x.style.display === "block") {
-    x.style.display = "none";
-} else {
+if (x.style.display === "none") {
     x.style.display = "block";
+} else {
+    x.style.display = "none";
+}
+}
+function kidFunction() {
+var x = document.getElementById("kids");
+if (x.style.display === "none") {
+    x.style.display = "block";
+} else {
+    x.style.display = "none";
 }
 }
